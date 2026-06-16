3dc3829468fc23d31b7823936929ea3bc5f61863
gokulan006 <gokulan.m.min24@itbhu.ac.in>
2026-06-16 20:38:38 +0530

    Update javascript .js

--- javascript .js
+++ javascript .js
@@ -1,5 +1,5 @@
 // Replace with your JSON file URL (GitHub Raw or Google Drive)
-const JSON_URL = "https://raw.githubusercontent.com/gokulan006/Medical/refs/heads/main/articles.json";
+const JSON_URL = "https://github.com/gokulan006/MedSnap/blob/6d064815d446199f669183091ef8322f11244c69/articles.json";
 //const JSON_URL = "https://raw.githubusercontent.com/gokulan006/Medical/main/articles.json";
 
 async function fetchArticles() {
